50be3e9dbdb396bdc9013ca34372332318dbdebf
向云飞 <978771447@qq.com>
2015-12-25 16:11:07 +0800

    创建项目

---
+++ Category/Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Category
+//
+//  Created by MyMac on 15/12/25.
+//  Copyright © 2015年 MyMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
